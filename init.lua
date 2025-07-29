2ace78df82f443a3c70d87dd0c456f81f840de68
Tjitte de Vries <post@vrieskist.com>
2025-07-29 23:37:49 +0200

    more plugins

--- init.lua
+++ init.lua
@@ -280,6 +280,50 @@ require('lazy').setup({
     },
   },
 
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
+
+  {
+    'windwp/nvim-ts-autotag',
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = {},
+  },
+
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
@@ -778,6 +822,7 @@ require('lazy').setup({
         languages = { 'vue' },
         configNamespace = 'typescript',
       }
+
       vim.lsp.config('vtsls', {
         settings = {
           vtsls = {
@@ -791,15 +836,7 @@ require('lazy').setup({
         filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
       })
 
-      vim.lsp.config('vue_ls', {
-        settings = {
-          init_options = {
-            typescript = {
-              tsdk = '',
-            },
-          },
-        },
-      })
+      vim.lsp.config('vue_ls', {})
 
       vim.lsp.enable 'vue_ls'
       vim.lsp.enable 'vtsls'
@@ -997,6 +1034,7 @@ require('lazy').setup({
       require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
+      -- TODO: Fix conflicting mapping S with Flash
       --
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
